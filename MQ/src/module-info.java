a77d72099ac0c52c4017adfaa73dc6239ceff7e4
ArunaDevi G <ArunaDevi G@DESKTOP-266DJFD>
2023-02-21 15:17:50 +0530

    demo

---
+++ MQ/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ArunaDevi G
+ *
+ */
+module MQ {
+}
---
+++ MQ/src/mq/Main.java
@@ -0,0 +1,8 @@
+package mq;
+
+public class Main {
+
+	public static void main(String[] args){
+		System.out.println("fg;oij");
+	}
+}
